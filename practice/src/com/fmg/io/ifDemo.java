5453bdbe3039c81bae7f08d933c463d06bcf18ee
fengmg <280575876@qq.com>
2017-04-10 09:42:42 +0800

    123

--- practice/src/com/fmg/io/ifDemo.java
+++ practice/src/com/fmg/io/ifDemo.java
@@ -1,16 +1,13 @@
 package com.fmg.io;
 
+import java.util.Date;
+
+import util.ConvertTimeUtil;
+
 public class ifDemo {
 
 	public static void main(String args[]) {
-		String b = "10";
-		if("10".equals(b)){
-			System.out.println("1111111111");
-		}else if(!b.equals("1")){
-			System.out.println("2222222222222");
-		}else{
-			System.out.println("33333333333");
-		}
+		System.out.println(ConvertTimeUtil.formatTime(new Date()));
 	}
 
 }
